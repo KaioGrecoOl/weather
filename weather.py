95bf431aadddfe9a9cea0bd6357e347f8ebb1807
Kaio G <kaiogreco91@gmail.com>
2024-03-06 12:31:17 -0300

    loop for 3 countrys

--- weather.py
+++ weather.py
@@ -4,50 +4,54 @@ from PIL import Image, ImageFont, ImageDraw
 from datetime import date 
 
 api_key = "9d0500XXXXXXXXXXXXXXX"
-city_list = ["Jaipur", "Delhi", "Mumbai", "Kolkata", "Chennai"]
+uk_list = ["London", "Manchester", "Edinburgh", "Bristol", "Birmingham"]
+india_list = ["Jaipur", "Delhi", "Mumbai", "Kolkata", "Chennai"]
+us_list = ["New York", "Chicago", "San Francisco", "Los Angeles", "San Diego"]
+
+country_list = [uk_list, india_list, us_list]
 position = [300, 430, 555, 690, 825]
 
 
-image = Image.open("post.png")
-draw = ImageDraw.Draw(image)
+for country in country_list:
+    image = Image.open("post.png")
+    draw = ImageDraw.Draw(image)
 
-font = ImageFont.truetype('Inter.ttf', size=50)
-content = "Latest Weather Forecast"
-color = 'rgb(255, 255, 255)'
-(x, y) = (55,50)
-draw.text((x, y), content, color, font=font)
+    font = ImageFont.truetype('Inter.ttf', size=50)
+    content = "Latest Weather Forecast"
+    color = 'rgb(255, 255, 255)'
+    (x, y) = (55,50)
+    draw.text((x, y), content, color, font=font)
 
-font = ImageFont.truetype('Inter.ttf', size=30)
-today = date.today()
-content = date.today().strftime("%A - %B %d, %Y")
-color = 'rgb(255, 255, 255)'
-(x, y) = (55,145)
-draw.text((x, y), content, color, font=font)
+    font = ImageFont.truetype('Inter.ttf', size=30)
+    today = date.today()
+    content = date.today().strftime("%A - %B %d, %Y")
+    color = 'rgb(255, 255, 255)'
+    (x, y) = (55,145)
+    draw.text((x, y), content, color, font=font)
 
-index = 0
-for city in city_list:
-        url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric".format(city, api_key)
-        response = requests.get(url)
-        data = json.loads(response.text)
+    index = 0
+    for city in country:
+            url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric".format(city, api_key)
+            response = requests.get(url)
+            data = json.loads(response.text)
 
-        font = ImageFont.truetype('Inter.ttf', size=50)
-        color = 'rgb(0, 0, 0)'
-        (x, y) = (135,position[index])
-        draw.text((x, y), city, color, font=font)
+            font = ImageFont.truetype('Inter.ttf', size=50)
+            color = 'rgb(0, 0, 0)'
+            (x, y) = (135,position[index])
+            draw.text((x, y), city, color, font=font)
 
-        font = ImageFont.truetype('Inter.ttf', size=50)
-        content = str(data['main']['temp']) + "\u00b0"
-        color = 'rgb(255, 255, 255)'
-        (x, y) = (600, position[index])
-        draw.text((x, y), content, color, font=font)
+            font = ImageFont.truetype('Inter.ttf', size=50)
+            content = str(data['main']['temp']) + "\u00b0"
+            color = 'rgb(255, 255, 255)'
+            (x, y) = (600, position[index])
+            draw.text((x, y), content, color, font=font)
 
-        font = ImageFont.truetype('Inter.ttf', size=50)
-        content = str(data['main']['humidity']) + "%"
-        color = 'rgb(255, 255, 255)'
-        (x, y) = (810, position[index])
-        draw.text((x, y), content, color, font=font)
+            font = ImageFont.truetype('Inter.ttf', size=50)
+            content = str(data['main']['humidity']) + "%"
+            color = 'rgb(255, 255, 255)'
+            (x, y) = (810, position[index])
+            draw.text((x, y), content, color, font=font)
 
-        index += 1
+            index += 1
 
-image.show()
-image.save("test.png")
+    image.save(str(date.today()) + country[0] + ".png")

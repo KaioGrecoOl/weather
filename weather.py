acff8b65ab25c32f2a0fdb59e254c90dd0b1cd15
Kaio G <kaiogreco91@gmail.com>
2024-03-06 11:41:23 -0300

    adding positions in a array and index

--- weather.py
+++ weather.py
@@ -4,11 +4,9 @@ from PIL import Image, ImageFont, ImageDraw
 from datetime import date 
 
 api_key = "9d0500XXXXXXXXXXXXXXX"
-city_list = ["Jaipur"]
-# uk_list = ["London", "Manchester", "Edinburgh", "Bristol", "Birmingham"]
-# india_list = ["Jaipur", "Delhi", "Mumbai", "Kolkata", "Chennai"]
-# us_list = ["New York", "Chicago", "San Francisco", "Los Angeles", "San Diego"]
-# country_list = [uk_list, india_list, us_list]
+city_list = ["Jaipur", "Delhi", "Mumbai", "Kolkata", "Chennai"]
+position = [300, 430, 555, 690, 825]
+
 
 image = Image.open("post.png")
 draw = ImageDraw.Draw(image)
@@ -26,6 +24,7 @@ color = 'rgb(255, 255, 255)'
 (x, y) = (55,145)
 draw.text((x, y), content, color, font=font)
 
+index = 0
 for city in city_list:
         url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric".format(city, api_key)
         response = requests.get(url)
@@ -33,20 +32,22 @@ for city in city_list:
 
         font = ImageFont.truetype('Inter.ttf', size=50)
         color = 'rgb(0, 0, 0)'
-        (x, y) = (135,300)
+        (x, y) = (135,position[index])
         draw.text((x, y), city, color, font=font)
 
         font = ImageFont.truetype('Inter.ttf', size=50)
         content = str(data['main']['temp']) + "\u00b0"
         color = 'rgb(255, 255, 255)'
-        (x, y) = (600, 300)
+        (x, y) = (600, position[index])
         draw.text((x, y), content, color, font=font)
 
         font = ImageFont.truetype('Inter.ttf', size=50)
         content = str(data['main']['humidity']) + "%"
         color = 'rgb(255, 255, 255)'
-        (x, y) = (810, 300)
+        (x, y) = (810, position[index])
         draw.text((x, y), content, color, font=font)
+
+        index += 1
 
 image.show()
 image.save("test.png")
